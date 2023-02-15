7709affaccc33d3716c870cb388d702ca0ec5e84
Samuel W <sam@cosmicbagel.com>
2023-02-15 00:56:14 -0700

    Adds 8bit compare with register instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -549,6 +549,7 @@ PC: 0x{:04x}",
             table[0x90 + i] = Cpu::sub_8bit_a_reg;
             table[0xa0 + i] = Cpu::and_8bit_a_reg;
             table[0xb0 + i] = Cpu::or_8bit_a_reg;
+            table[0xb8 + i] = Cpu::cp_8bit_a_reg;
         }
 
         table[0xcd] = Cpu::call;
@@ -711,7 +712,7 @@ PC: 0x{:04x}",
         let (result, overflow_occurred) = self.a.overflowing_sub(val);
 
         //update flags
-        self.f |= N_FLAG_MASK; // always flip N flag on 
+        self.f |= N_FLAG_MASK; // always flip N flag on
 
         //is result zero flag
         if result == 0 {
@@ -794,6 +795,48 @@ PC: 0x{:04x}",
 
         //store result
         self.a = result;
+
+        if from_reg == 6 {
+            8
+        } else {
+            4
+        }
+    }
+
+    fn cp_8bit_a_reg(&mut self, opcode: u8) -> CycleCount {
+        //0b10010_xxx
+        let from_reg = opcode & 0b00_000_111;
+
+        let val = self.read_reg(from_reg);
+        let (result, overflow_occurred) = self.a.overflowing_sub(val);
+
+        //update flags
+        self.f |= N_FLAG_MASK; // always flip N flag on
+
+        //is result zero flag
+        if result == 0 {
+            self.f |= Z_FLAG_MASK; // flip on
+        } else {
+            self.f &= !Z_FLAG_MASK; // flip off
+        }
+
+        //carry flag
+        if overflow_occurred {
+            self.f |= C_FLAG_MASK; // flip on
+        } else {
+            self.f &= !C_FLAG_MASK; // flip off
+        }
+
+        //half carry flag
+        //mask out upper nibble and see if result flips 0x10 bit (meaning
+        //there was a half carry as result was greater than 0x0f)
+        if (self.a & 0xf).wrapping_sub(val & 0xf) & 0x10 == 0 {
+            self.f |= H_FLAG_MASK; // flip on
+        } else {
+            self.f &= !H_FLAG_MASK; // flip off
+        }
+
+        //do NOT store the result (this is compare)
 
         if from_reg == 6 {
             8
